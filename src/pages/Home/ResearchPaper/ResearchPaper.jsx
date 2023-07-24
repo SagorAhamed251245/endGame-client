283c865820c9a3a94614ca43275531083760d5f6
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-07-24 15:56:08 +0600

    recherch papper

--- src/pages/Home/ResearchPaper/ResearchPaper.jsx
+++ src/pages/Home/ResearchPaper/ResearchPaper.jsx
@@ -1,45 +1,22 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle";
 
 const ResearchPaper = () => {
-  const Papers = [
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-    {
-      img: "https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp",
-    },
-  ];
+  const [Papers, setPapers] = useState([]);
+
+  useEffect(() => {
+
+    fetch("resherchPaper.json")
+      .then((res) => res.json())
+      .then((data) => setPapers(data));
+  }, [Papers]);
   return (
     <>
       <SectionTitle>Research Papers</SectionTitle>
-      <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 overflow-hidden">
-        <div className="-m-1 flex flex-wrap md:-m-2 overflow-hidden">
+      <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 overflow-hidden ">
+        <div className="-m-1 flex flex-wrap justify-center md:-m-2 overflow-hidden gap-2 ">
           {Papers.map((paper, index) => (
-            <div key={index} className="flex w-1/3 flex-wrap  overflow-hidden">
+            <div key={index} className="flex w-1/3 flex-wrap  overflow-hidden border border-2">
               <div className="w-full  p-1 md:p-2 overflow-hidden rounded-lg">
                 <img
                   alt="gallery"
